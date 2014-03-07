86dbfc328b6bd4cf9d7e17d986a70016986bcc02
mlewisno <mlewisno@gmail.com>
2014-03-06 21:57:28 -0500

    Added some new menus
    Added basic scenes for the various options menus, adding menu options
    now, as well as global variables to store the relevant information

---
+++ Assets/Scripts/OptionMenuScript.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionMenuScript : MonoBehaviour
+{
+
+	public Font ourFont;
+	
+	void OnGUI()
+	{
+		const int buttonWidth = 300;
+		const int buttonHeight = 40;
+		
+		GUI.backgroundColor = Color.black;
+		GUI.skin.font = ourFont;
+		
+		// Draw a button to start the game
+		if (
+			GUI.Button(
+			// Center in X, 2/3 of the height in Y
+			new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(2 * Screen.height / 4) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			),
+			"Play Pong!"
+			)
+			)
+		{
+			// On Click, load the first level.
+			// "Stage1" is the name of the first scene we created.
+			Application.LoadLevel("Stage1");
+		}
+		
+		// Draw a button to open the options menu
+		if (
+			GUI.Button(
+			// Center in X, 2/3 of the height in Y
+			new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(2 * Screen.height / 4) + (buttonHeight * 1.15f) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			),
+			"Player Controls"
+			)
+			)
+		{
+			// On Click, roll the credits
+			Application.LoadLevel("ControlsMenu");
+		}
+		
+		// Draw a button to play the credits
+		if (
+			GUI.Button(
+			// Center in X, 2/3 of the height in Y
+			new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(2 * Screen.height / 4) + (buttonHeight * 1.15f) + (buttonHeight / 2 + buttonHeight*0.15f),
+			buttonWidth,
+			buttonHeight
+			),
+			"Audio"
+			)
+			)
+		{
+			// On Click, roll the credits
+			Application.LoadLevel("Sound");
+		}
+		
+		// Draw a button to close the game
+		if (
+			GUI.Button(
+			// Center in X, 2/3 of the height in Y
+			new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(2 * Screen.height / 4) + (buttonHeight * 1.15f) + (buttonHeight * 1.65f),
+			buttonWidth,
+			buttonHeight
+			),
+			"Back to Main Menu"
+			)
+			)
+		{
+			// On Click, close the game.
+			Application.LoadLevel ("Menu");
+		}
+	}
+	
+}

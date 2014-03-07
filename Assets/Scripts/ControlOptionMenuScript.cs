51a2481ae0e5fdc4a51fb48cd7dcf8c91d53e8e3
mlewisno <mlewisno@gmail.com>
2014-03-06 23:01:21 -0500

    Began vary preliminary control menu coding
    Very very preliminary

---
+++ Assets/Scripts/ControlOptionMenuScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlOptionMenuScript : MonoBehaviour
+{
+	public Font ourFont;	
+	private int hSliderValue = 4;
+	
+	void OnGUI () {
+		hSliderValue = GUI.HorizontalSlider (new Rect (25, 25, 100, 30), hSliderValue, 2, 4);
+	}
+	
+}

a2a021951958d951c128bde8ac14ba16933da406
mlewisno <mlewisno@gmail.com>
2014-03-11 10:50:49 -0400

    Added more of the option menu items
    Working on reformatting the visuals for the options, but they link to
    what they need to

--- Assets/Scripts/ControlOptionMenuScript.cs
+++ Assets/Scripts/ControlOptionMenuScript.cs
@@ -8,52 +8,49 @@ public class ControlOptionMenuScript : MonoBehaviour
 	const int buttonWidth = 500;
 	const int buttonHeight = 100;
 
+	// Keeps track of slider position
+	private float hSliderValue = 20.0f;
+
 	void OnGUI () {
+		// This area handles the general game settings
+		GUILayout.BeginArea (new Rect (Screen.width/4, Screen.height/4, 300, 300));
 		// This checkbox enables 4 players
-		SettingsVariables.four_players = GUI.Toggle 
-					(new Rect (Screen.width / 2 - (buttonWidth / 2),
-        			(2 * Screen.height / 4) - (buttonHeight * 1.15f) - (buttonHeight / 2),
-       				buttonWidth,
-        			buttonHeight), 
-				SettingsVariables.four_players, "Enable 4 Players");
+		SettingsVariables.four_players = GUILayout.Toggle (SettingsVariables.four_players, "Enable 4 Players");
+
+		GUILayout.EndArea ();
+
+		// This area handles the individual player settings
+		GUILayout.BeginArea (new Rect (Screen.width/2, Screen.height/2, 300, 300));
 
 		// This sliding bar changes the player speed 
+		GUILayout.Label ("Sensetivity");
+		SettingsVariables.teamOnePlayerOneSpeed = GUILayout.HorizontalSlider (SettingsVariables.teamOnePlayerOneSpeed, 20.0f, 30.0f);
+
+		GUILayout.EndArea ();
+
+		// This area holds the menu options to change menus
+		GUILayout.BeginArea (new Rect (0, 0, Screen.width, 100));
+		GUILayout.BeginHorizontal ();
 
 		// Draw a button to go back to the options menu
-		if (
-			GUI.Button(
-			// Center in X, 2/3 of the height in Y
-			new Rect(
-			Screen.width / 2 - (buttonWidth / 2),
-			(2 * Screen.height / 4) - (buttonHeight * 1.15f),
-			buttonWidth,
-			buttonHeight
-			),
-			"Back to Options Menu"
-			)
-			)
+		if (GUILayout.Button("Options Menu"))
 		{
 			// On Click, close the game.
 			Application.LoadLevel ("OptionsMenu");
 		}
 
 		// Draw a button to go back to the main menu
-		if (
-			GUI.Button(
-			// Center in X, 2/3 of the height in Y
-			new Rect(
-			Screen.width / 2 - (buttonWidth / 2),
-			(2 * Screen.height / 4) - (buttonHeight * 1.15f) + (buttonHeight * 1.65f),
-			buttonWidth,
-			buttonHeight
-			),
-			"Back to Main Menu"
-			)
-			)
+		if (GUILayout.Button("Main Menu"))
 		{
 			// On Click, close the game.
 			Application.LoadLevel ("Menu");
 		}
+		GUILayout.EndHorizontal ();
+		GUILayout.EndArea ();
+	}
+
+	void Update(){
+		Debug.Log (hSliderValue);
 	}
 	
 }

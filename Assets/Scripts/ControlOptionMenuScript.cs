28d748d14c7c0d7c0f8e3add04819e5163da2816
mlewisno <mlewisno@gmail.com>
2014-03-11 16:44:50 -0400

    Menu is getting so much better

--- Assets/Scripts/ControlOptionMenuScript.cs
+++ Assets/Scripts/ControlOptionMenuScript.cs
@@ -19,13 +19,28 @@ public class ControlOptionMenuScript : MonoBehaviour
 		GUILayout.BeginArea( new Rect (Screen.width * 0.1f, Screen.height * 0.1f, Screen.width, 0.9f * Screen.height));
 			GUILayout.BeginHorizontal ();
 				// Sets up the left section of the menu which handles general settings
-				GUILayout.BeginArea( new Rect (0, 0, Screen.width/2, 0.9f * Screen.height));
+		GUILayout.BeginArea( new Rect (0, 0, Screen.width/2 - Screen.width*0.1f, 0.9f * Screen.height));
 					SettingsVariables.four_players = GUILayout.Toggle (SettingsVariables.four_players, "Enable 4 Players");
 				GUILayout.EndArea();
-
-				GUILayout.BeginArea( new Rect (Screen.width/2, 0, Screen.width/2, 0.9f * Screen.height));
-					toolbarInt = GUILayout.Toolbar (toolbarInt, toolbarStrings);
+				
+				
+				GUILayout.BeginArea( new Rect (Screen.width/2 - Screen.width*0.1f, 0, Screen.width/2 - Screen.width*0.1f, 0.9f * Screen.height));
+					GUILayout.BeginVertical ();
+						// Label for the section
+						GUILayout.Label("Player Settings");
+						// Toolbar for selecting which player's settings to change
+						toolbarInt = GUILayout.Toolbar (toolbarInt, toolbarStrings);
+						// Label and slider for the first ediutable parameter, player sensetivity
+						GUILayout.Label ("Sensetivity");
+						SettingsVariables.teamOnePlayerOneSpeed = GUILayout.HorizontalSlider (SettingsVariables.teamOnePlayerOneSpeed, 20.0f, 30.0f);
+						// Label and entry box for the Up Key
+						GUILayout.Label ("Up Key");
+						
+						// Label and entry box for the Down Key
+						GUILayout.Label ("Down Key");
+					GUILayout.EndVertical ();
 				GUILayout.EndArea();
+
 
 			GUILayout.EndHorizontal ();
 		GUILayout.EndArea();
@@ -52,8 +67,7 @@ public class ControlOptionMenuScript : MonoBehaviour
 		GUILayout.BeginArea (new Rect (Screen.width/2, Screen.height/4, 300, 300));
 
 		// This sliding bar changes the player speed 
-		GUILayout.Label ("Sensetivity");
-		SettingsVariables.teamOnePlayerOneSpeed = GUILayout.HorizontalSlider (SettingsVariables.teamOnePlayerOneSpeed, 20.0f, 30.0f);
+
 
 		GUILayout.EndArea ();
 	}

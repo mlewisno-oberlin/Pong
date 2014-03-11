adbc1955c9baa7d4d74395b57d352077501a19dc
mlewisno <mlewisno@gmail.com>
2014-03-11 16:28:58 -0400

    Fixed organization of menu objects
    Began building a menu layout structure

--- Assets/Scripts/ControlOptionMenuScript.cs
+++ Assets/Scripts/ControlOptionMenuScript.cs
@@ -7,50 +7,59 @@ public class ControlOptionMenuScript : MonoBehaviour
 	public GUISkin skin;
 	const int buttonWidth = 500;
 	const int buttonHeight = 100;
+	private int toolbarInt = 0;
+	private string[] toolbarStrings = {"P1", "P2", "P3", "P4"};
 
 	// Keeps track of slider position
 	private float hSliderValue = 20.0f;
 
 	void OnGUI () {
-		// This area handles the general game settings
-		GUILayout.BeginArea (new Rect (Screen.width/4, Screen.height/4, 300, 300));
-		// This checkbox enables 4 players
-		SettingsVariables.four_players = GUILayout.Toggle (SettingsVariables.four_players, "Enable 4 Players");
+		// Sets up the two highest level areas
+		// Area One
+		GUILayout.BeginArea( new Rect (Screen.width * 0.1f, Screen.height * 0.1f, Screen.width, 0.9f * Screen.height));
+			GUILayout.BeginHorizontal ();
+				// Sets up the left section of the menu which handles general settings
+				GUILayout.BeginArea( new Rect (0, 0, Screen.width/2, 0.9f * Screen.height));
+					SettingsVariables.four_players = GUILayout.Toggle (SettingsVariables.four_players, "Enable 4 Players");
+				GUILayout.EndArea();
 
-		GUILayout.EndArea ();
+				GUILayout.BeginArea( new Rect (Screen.width/2, 0, Screen.width/2, 0.9f * Screen.height));
+					toolbarInt = GUILayout.Toolbar (toolbarInt, toolbarStrings);
+				GUILayout.EndArea();
 
-		// This area handles the individual player settings
-		GUILayout.BeginArea (new Rect (Screen.width/2, Screen.height/2, 300, 300));
+			GUILayout.EndHorizontal ();
+		GUILayout.EndArea();
+
+		// Area Two
+		GUILayout.BeginArea (new Rect (0, Screen.height * 0.9f, Screen.width, Screen.height * 0.1f));
+			GUILayout.BeginHorizontal ();
+				// Draw a button to go back to the options menu
+				if (GUILayout.Button("Options Menu"))
+				{
+					// On Click, close the game.
+					Application.LoadLevel ("OptionsMenu");
+				}
+				
+				// Draw a button to go back to the main menu
+				if (GUILayout.Button("Main Menu"))
+				{
+					// On Click, close the game.
+					Application.LoadLevel ("Menu");
+				}
+			GUILayout.EndHorizontal ();
+		GUILayout.EndArea();
+
+		GUILayout.BeginArea (new Rect (Screen.width/2, Screen.height/4, 300, 300));
 
 		// This sliding bar changes the player speed 
 		GUILayout.Label ("Sensetivity");
 		SettingsVariables.teamOnePlayerOneSpeed = GUILayout.HorizontalSlider (SettingsVariables.teamOnePlayerOneSpeed, 20.0f, 30.0f);
 
 		GUILayout.EndArea ();
-
-		// This area holds the menu options to change menus
-		GUILayout.BeginArea (new Rect (0, 0, Screen.width, 100));
-		GUILayout.BeginHorizontal ();
-
-		// Draw a button to go back to the options menu
-		if (GUILayout.Button("Options Menu"))
-		{
-			// On Click, close the game.
-			Application.LoadLevel ("OptionsMenu");
-		}
-
-		// Draw a button to go back to the main menu
-		if (GUILayout.Button("Main Menu"))
-		{
-			// On Click, close the game.
-			Application.LoadLevel ("Menu");
-		}
-		GUILayout.EndHorizontal ();
-		GUILayout.EndArea ();
 	}
 
 	void Update(){
-		Debug.Log (hSliderValue);
+
 	}
 	
 }
